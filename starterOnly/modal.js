083986680a27cb5c923f7be7979d1fde8ab7d746
Mickael <mickael.krauth@epita.fr>
2022-09-06 14:08:54 +0200

    Site ready on Chrome

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -64,10 +64,10 @@ function closeModal() {
 function checkFirstName() {
   text = document.getElementById("first");
   if (text.value.length < 2) {
-    document.querySelector(".checkName").style.display = "block";
+    toggleError(".checkName");
     firstNameBool = false;
   } else if (text.value.length >= 2) {
-    document.querySelector(".checkName").style.display = "none";
+    toggleError(".checkName", "none");
     firstNameBool = true;
   }
 }
@@ -75,10 +75,10 @@ function checkFirstName() {
 function checkLastName() {
   text = document.getElementById("last");
   if (text.value.length < 2) {
-    document.querySelector(".checkLast").style.display = "block";
+    toggleError(".checkLast");
     LastNameBool = false;
   } else if (text.value.length >= 2) {
-    document.querySelector(".checkLast").style.display = "none";
+    toggleError(".checkLast", "none");
     LastNameBool = true;
   }
 }
@@ -86,21 +86,22 @@ function checkLastName() {
 function checkEmail() {
   text = document.getElementById("email");
   if (isEmail(text.value)) {
-    document.querySelector(".checkEmail").style.display = "none";
+    toggleError(".checkEmail", "none");
     emailBool = true;
   } else {
-    document.querySelector(".checkEmail").style.display = "block";
+    toggleError(".checkEmail");
     emailBool = false;
   }
 }
 
 function checkNum() {
   text = document.getElementById("quantity");
-  if (isNaN(Number(text.value))) {
-    document.querySelector(".checkQuantity").style.display = "block";
+  console.log("Calling checknum", text.value);
+  if (!text.value.length || isNaN(text.value)) {
+    toggleError(".checkQuantity");
     isNum = false;
   } else {
-    document.querySelector(".checkQuantity").style.display = "none";
+    toggleError(".checkQuantity", "none");
     isNum = true;
   }
 }
@@ -114,10 +115,42 @@ function validRadio() {
   }
   return false;
 }
-
+function toggleError(selector, display = "block") {
+  document.querySelector(selector).style.display = display;
+}
 //Check if the form is valid
 function validate() {
   event.preventDefault();
+  if (!firstNameBool) {
+    toggleError(".checkName");
+    alert("Please enter a valid first name");
+    return false;
+  }
+
+  if (!LastNameBool) {
+    toggleError(".checkLast");
+    alert("Please enter a valid last name");
+    return false;
+  }
+  if (!emailBool) {
+    toggleError(".checkEmail");
+    alert("Please enter a valid email");
+    return false;
+  }
+  if (document.getElementById("birthdate").value === "") {
+    // alert("Please enter a birth date");
+    toggleError(".checkDate");
+    return false;
+  } else {
+    toggleError(".checkDate", "none");
+  }
+
+  if (!isNum) {
+    alert("Please enter a valid number of tourneys");
+    toggleError(".checkQuantity");
+    return false;
+  }
+
   if (!validRadio()) {
     alert("Please check at least one radio button");
     return false;
@@ -126,35 +159,9 @@ function validate() {
     alert("Please accept the terms of service");
     return false;
   }
-  if (document.getElementById("birthdate").value === "") {
-    // alert("Please enter a birth date");
-    document.querySelector(".checkDate").style.display = "block";
-    return false;
-  } else {
-    document.querySelector(".checkDate").style.display = "none";
-  }
-  if (!emailBool) {
-    alert("Please enter a valid email");
-    return false;
-  }
-  if (!firstNameBool) {
-    alert("Please enter a valid first name");
-    return false;
-  }
-
-  if (!LastNameBool) {
-    alert("Please enter a valid last name");
-    return false;
-  }
-
-  if (!isNum) {
-    alert("Please enter a valid number of tourneys");
-    document.querySelector(".checkQuantity").style.display = "block";
-    return false;
-  }
 
   document.querySelector(".modal-cl").style.display = "none";
   document.querySelector(".modal-confirm").style.display = "block";
-
+  document.querySelector("form").reset();
   return true;
 }
